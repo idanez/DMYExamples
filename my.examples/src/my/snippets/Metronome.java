e81fc49a057585e235a148651535f393783cb9d2
daniel.yampolschi <daniel.yampolschi@172.16.70.61>
2016-07-22 19:14:29 -0300

    asdfasdfasdf

--- my.examples/src/my/snippets/Metronome.java
+++ my.examples/src/my/snippets/Metronome.java
@@ -3,20 +3,24 @@ package my.snippets;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
+import javax.sound.midi.ControllerEventListener;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MetaEventListener;
 import javax.sound.midi.MetaMessage;
+import javax.sound.midi.MidiChannel;
 import javax.sound.midi.MidiEvent;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
 import javax.sound.midi.ShortMessage;
+import javax.sound.midi.Synthesizer;
 import javax.sound.midi.Track;
 
 public class Metronome implements MetaEventListener {
 	private Sequencer sequencer;
 	private int bpm;
+	private Synthesizer synthesizer;
 
 	public static void main(final String[] args) throws Exception {
 		int bpm = 120;
@@ -46,8 +50,23 @@ public class Metronome implements MetaEventListener {
 
 	private void openSequencer() throws MidiUnavailableException {
 		sequencer = MidiSystem.getSequencer();
+		synthesizer = MidiSystem.getSynthesizer();
 		sequencer.open();
 		sequencer.addMetaEventListener(this);
+		int[] controllersOfInterest = { 1, 4 };
+		sequencer.addControllerEventListener(new ControllerEventListener() {
+
+			@Override
+			public void controlChange(final ShortMessage event) {
+				System.out.println(event.getData1());
+				MidiChannel channel = synthesizer.getChannels()[9];
+				if (event.getData1() == 38) {
+					channel.setPitchBend(1000);
+				} else {
+					channel.setPitchBend(8192);
+				}
+			}
+		}, controllersOfInterest);
 	}
 
 	private Sequence createSequence() {
@@ -59,10 +78,14 @@ public class Metronome implements MetaEventListener {
 			MidiEvent evt = new MidiEvent(msg, 0);
 			track.add(evt);
 
-			addNoteEvent(track, 0);
-			addNoteEvent(track, 1);
-			addNoteEvent(track, 2);
-			addNoteEvent(track, 3);
+			int note = 37;
+			int channel = 9; // 9 reserved for percursion
+			int velocity = 127;
+
+			addNoteEvent(track, 1, channel, note, velocity);
+			addNoteEvent(track, 2, channel, note, velocity);
+			addNoteEvent(track, 3, channel, note, velocity);
+			addNoteEvent(track, 4, channel, 38, velocity);
 
 			msg = new ShortMessage(ShortMessage.PROGRAM_CHANGE, 9, 1, 0);
 			evt = new MidiEvent(msg, 4);
@@ -74,8 +97,9 @@ public class Metronome implements MetaEventListener {
 		}
 	}
 
-	private void addNoteEvent(final Track track, final long tick) throws InvalidMidiDataException {
-		ShortMessage message = new ShortMessage(ShortMessage.NOTE_ON, 9, 37, 100);
+	private void addNoteEvent(final Track track, final long tick, final int channel, final int note, final int velocity)
+			throws InvalidMidiDataException {
+		ShortMessage message = new ShortMessage(ShortMessage.NOTE_ON, channel, note, velocity);
 		MidiEvent event = new MidiEvent(message, tick);
 		track.add(event);
 	}
@@ -88,6 +112,7 @@ public class Metronome implements MetaEventListener {
 
 	@Override
 	public void meta(final MetaMessage message) {
+		System.out.println(message.getStatus());
 		if (message.getType() != 47) { // 47 is end of track
 			return;
 		}
